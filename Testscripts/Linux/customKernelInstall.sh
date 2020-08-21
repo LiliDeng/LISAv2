fe54ff31c4ed99d54be8d492c2ccd6e2fc77ed87
Lili Deng <lildeng@microsoft.com>
2020-08-21 12:18:09 +0800

    Fix

--- Testscripts/Linux/customKernelInstall.sh
+++ Testscripts/Linux/customKernelInstall.sh
@@ -254,6 +254,7 @@ function InstallKernel() {
         apt-get -y update >> $LOG_FILE 2>&1
         CheckInstallLockUbuntu
         apt-get install -yq linux-azure/"$release" >> $LOG_FILE 2>&1
+        update-grub
         kernelInstallStatus=$?
         if [ $kernelInstallStatus -ne 0 ]; then
             LogMsg "CUSTOM_KERNEL_FAIL"
